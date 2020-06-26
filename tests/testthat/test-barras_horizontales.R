2b9945d39e174af8d75662469b14127a5579bf73
Rafael La Buonora <info@rlabuonora.com>
2020-06-26 14:01:35 -0300

    Add tests for barras_horizontales

---
+++ tests/testthat/test-barras_horizontales.R
@@ -0,0 +1,31 @@
+test_that("Grafico 10", {
+
+  df <- tibble::tribble(
+    ~depto, ~lineas,
+    "Artigas",   16956,
+    "Canelones",  164294,
+    "Cerro Largo",   21300,
+    "Colonia",   43492,
+    "Durazno",   14413,
+    "Flores",    7943,
+    "Florida",   19332,
+    "Lavalleja",   18845,
+    "Maldonado",   89297,
+    "Montevideo",  556391,
+    "Paysandú",   29095,
+    "Río Negro",   14355,
+    "Rivera",   25133,
+    "Rocha",   25041,
+    "Salto",   28940,
+    "San José",   31719,
+    "Soriano",   23361,
+    "Tacuarembó",   22538,
+    "Treinta y Tres",   12928
+  )
+
+  p <- barras_horizontales(lineas, depto, nudge_text = 5e4)
+  expect_true(ggplot2::is.ggplot(p))
+  expect_true("GeomCol" %in% class(p$layers[[1]]$geom))
+  expect_true("GeomText" %in% class(p$layers[[2]]$geom))
+
+})
