fb68d6b43cc46d33f10ae3d90539a36022ac385e
Rafael La Buonora <info@rlabuonora.com>
2020-10-12 22:18:43 -0300

    auto nudge barras_horizontales

--- R/barras_horizontales.R
+++ R/barras_horizontales.R
@@ -14,6 +14,9 @@ barras_horizontales <- function(df, valor, cat, nudge_text=0, espacio_extra=0.15
   valor_quo <- rlang::enquo(valor)
   cat_quo <- rlang::enquo(cat)
 
+  valores <- dplyr::pull(df, !! valor_quo)
+  auto_nudge <- max(valores) * 0.08
+
   df %>%
     ggplot2::ggplot(ggplot2::aes(forcats::fct_reorder(!!cat_quo, !! valor_quo), !! valor_quo)) +
     ggplot2::geom_col(fill = pal[5], width = 0.5) +
